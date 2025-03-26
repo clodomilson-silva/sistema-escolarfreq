36c7a6c1b3dbe3cf39c540b285b314615aeba5d5
Clodomilson <clodomilsonanjos.eng@outlook.com>
2025-03-26 17:30:25 -0300

    "feat: adicionar modelo Turma ao backend"

--- backend/turmas/models.py
+++ backend/turmas/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class Turma(models.Model):
     nome = models.CharField(max_length=100)
     ano = models.IntegerField()
-    
+    turno = models.CharField(
+        max_length=10,
+        choices=[("Manhã", "Manhã"), ("Tarde", "Tarde"), ("Noite", "Noite")],
+        null = True
+    )
+
     def __str__(self):
-        return f"{self.nome} - {self.ano}"
+        return f"{self.nome} - {self.ano} ({self.turno})"
